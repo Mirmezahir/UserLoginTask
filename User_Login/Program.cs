564635d01f7fa2f0125d6c2cb7c8a4a8db34a2d5
Mirmezahir <mirmazahirmi-bp215@code.edu.az>
2024-10-21 23:45:02 +0400

    UserLogin

---
+++ User_Login/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace User_Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cixis= false;
+            bool deyisen = false;
+            User user1= null;
+            do
+            {
+                
+                Console.WriteLine("                           [ USER LOGIN ]                       ");
+
+                Console.Write("Fullname : ");
+                string name = Console.ReadLine();
+                name.Trim();
+                Console.Write("E-mail : ");
+                string email = Console.ReadLine();
+                email.Trim();
+                Console.WriteLine("Sifre teyin edin !\n-Sifre en az 8 simvol ibaret olmalidir\n-En az bir reqem olmalidir\n-En az bir boyuk" +
+                    "herf olmalidir\n-En az bir kicik herf olmalidir\n-En az bir reqem olmalidir!!!");
+                Console.Write("Password : ");
+                do
+                {
+                    string password = Console.ReadLine();
+                    password.Trim();
+                   user1 = new User(name, email, password);
+                    user1.PasswordCheck(password);
+                   
+                } while (!user1.IsPasswordCorrect);
+                User user = user1;
+              
+                Console.WriteLine("Press 0 for EXIT\nPress 1 for Information about User");
+                int secim;
+                do
+                {
+                  deyisen = int.TryParse(Console.ReadLine(), out secim);
+
+                    switch (secim)
+                    {
+                        case 0: cixis = true;     deyisen = true; break;
+
+
+
+                            case 1:
+                            user.ShowfullInfo(user);
+                    break;
+                    
+                    
+                    }
+
+
+
+
+                } while (!deyisen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            } while (!cixis);
+            
+        }
+    }
+}
---
+++ User_Login/User.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Login
+{
+    internal class User:IAccount
+    {
+        User[] Users= new User[0];
+       private int _id;
+        public int ID { get; set; }
+        public string FullName;
+        public string Email;
+        public string Password;
+        public bool IsPasswordCorrect;
+
+        public User(string fullName, string email, string password)
+        {
+            ++_id;
+            ID = _id;
+            FullName = fullName;
+            Email = email;
+            Password = password;
+            
+        }
+        public void UserADD(User user)
+        {
+            Array.Resize(ref Users, +1);
+            Users[Users.Length-1] = user;
+        }
+        
+        public void ShowfullInfo(User user)
+        {
+            Console.WriteLine($"ID :{user.ID}\nFULLNAME : {user.FullName}\nEMAIL :{user.Email}\nPASSWORD:{user.Password}");
+            
+        }
+
+        public bool PasswordCheck(string password)
+        {
+            bool correctpassword=false;
+            int boyukherf = 0;
+            int kicikherf = 0;
+            int reqem = 0;
+            
+            
+                
+                
+                for (int i = 0;i<password.Length;i++)
+                {
+                    
+                    if (Char.IsDigit(password[i]))
+                    {
+                        reqem ++;
+                    }
+                    if (Char.IsUpper(password[i]))
+                    {
+                        boyukherf ++;
+                    }
+                    if(Char.IsLower(password[i]))
+                    {
+                        kicikherf ++;
+                    }
+                    
+                }
+            if (password.Length < 7)
+            {
+                Console.WriteLine("Sifre de minimum 8 simvol olmalidir");
+
+
+            }
+            else if (reqem < 1)
+            {
+                Console.WriteLine("Sifrede en az bir reqem olmalidi");
+               
+            }
+            else if (boyukherf < 1)
+            {
+                Console.WriteLine("Sifrede en az bir boyuk herf olmalidi");
+                
+            }
+            else if (kicikherf < 1)
+            {
+                Console.WriteLine("Sifrede en az bir kicik herf olmalidi");
+                
+            }
+            else
+            {
+                Console.WriteLine("Sifreniz teyin olundu");
+                correctpassword = true;
+               
+            }
+            IsPasswordCorrect=correctpassword;  
+
+
+            return IsPasswordCorrect;
+        }
+    }
+}
